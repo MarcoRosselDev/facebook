191845f7c221b90dd47eeef33c80536c14f3cb68
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-05 19:57:30 -0300

    middle header component and media q in tailwind

--- components/Header.js
+++ components/Header.js
@@ -7,20 +7,21 @@ import { BsCart3 } from "react-icons/bs";
 import { GrGroup, GrAppsRounded } from "react-icons/gr";
 import { FaBell } from "react-icons/fa";
 import { AiOutlineMessage } from "react-icons/ai";
+import user from "../assets/guy7.jpg";
 
 const Header = () => {
   return (
-    <div className="p-5 flex items-center justify-between">
+    <div className="p-5 flex items-center justify-between border-b">
       {/* left Side */}
       <div className="flex items-center">
         <div className="w-12 h-12">
-          <Image src={facebook}></Image>
+          <Image src={facebook} alt="logo of facebook"></Image>
         </div>
         <div className="ml-2">
           <input
             type="text"
             placeholder="Search Facebook"
-            className="ml-2 outline-0 bg-[#f2f3f7] p-2 rounded-full pl-4"
+            className="ml-2 outline-0 bg-[#f2f3f7] p-2 rounded-full pl-4 hidden sm:block"
           />
         </div>
       </div>
@@ -33,6 +34,16 @@ const Header = () => {
         <GrGroup className="w-7 h-7" />
       </div>
       {/* Right Side */}
+      <div className="flex space-x-7 items-center">
+        <div className=" space-x-6 hidden lg:flex">
+          <GrAppsRounded className="w-7 h-7" />
+          <FaBell className="w-7 h-7" />
+          <AiOutlineMessage className="w-7 h-7" />
+        </div>
+        <div className="w-10 h-10">
+          <Image src={user} className="rounded-full" alt="user image" />
+        </div>
+      </div>
     </div>
   );
 };
