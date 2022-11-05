9161649c5fe4d7debbc4c3c63622d43f68e01342
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-05 20:30:17 -0300

    Feed component structure

---
+++ components/Feed.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Feed = () => {
+  return (
+    <div>
+      {/* Left Side Bar */}
+      {/* Stories */}
+      {/* CreatePost */}
+      {/* Posts */}
+      {/* Right Side Bar */}
+    </div>
+  );
+};
+
+export default Feed;
