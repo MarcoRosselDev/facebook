04a4f952197447fedcc80f5838797d5b5ce4c1d5
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-05 16:42:58 -0300

    header component 2

--- components/Header.js
+++ components/Header.js
@@ -4,10 +4,17 @@ import facebook from "../assets/facebook1.png";
 
 const Header = () => {
   return (
-    <div>
-      <div className="">
+    <div className="p-5">
+      <div className="flex items-center">
         <div className="w-12 h-12">
           <Image src={facebook}></Image>
+        </div>
+        <div className="ml-2">
+          <input
+            type="text"
+            placeholder="Search Facebook"
+            className="ml-2 outline-0 bg-[#f2f3f7] p-2 rounded-full pl-4"
+          />
         </div>
       </div>
     </div>
