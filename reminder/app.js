3c2fe5b5b9d60bf8a8a4cf00fa2aed3a633a6274
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-07 02:22:04 -0300

    js 4

--- reminder/app.js
+++ reminder/app.js
@@ -1,41 +1,28 @@
-/*
-<input type="text" class="input" />
-    <button class="add-button">add</button>
-    <button class="delete-button">delete</button>
-    <button class="clean-button">clean all</button>
-    <!-- modifiqued dom -->
-    <div class="div-dom"></div>
-
-*/
-
 const input = document.querySelector("input");
 const buttonAdd = document.querySelector(".add-button");
 const buttonDelete = document.querySelector(".delete-button");
 const buttonClean = document.querySelector(".clean-button");
 const divDom = document.querySelector(".div-dom");
+const areInLocalStorage = JSON.parse(localStorage.getItem("exampleList"));
 let inputValue = [];
 
-let isLocalHost = [];
+if (areInLocalStorage) {
+  inputValue = areInLocalStorage;
+  printDom(inputValue);
+}
 
 buttonAdd.addEventListener("click", function () {
   inputValue.push(input.value);
+  console.log("holas");
+  localStorage.setItem("exampleList", JSON.stringify(inputValue));
   input.value = "";
   printDom(inputValue);
 });
 
-const printDom = (arr) => {
+function printDom(arr) {
   let printInner = "";
   for (let i = 0; i < arr.length; i++) {
     printInner += `<li>${arr[i]}</li>`;
   }
   divDom.innerHTML = printInner;
-};
-
-/*
-  inputValue = input.value;
-  for (let i = 0; i < inputValue.length; i++) {
-    inputValue.push() = `<li>${inputValue[i]}</li>`;
-  }
-
-  divDom.innerHTML = inputValue;
-*/
+}
