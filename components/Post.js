6454b03c1e8cb4205f80d47f567df38ca83e27c3
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-07 02:52:23 -0300

    Post and Posts component structure

--- components/Post.js
+++ components/Post.js
@@ -13,7 +13,34 @@ import { BiWorld } from "react-icons/bi";
 import Image from "next/image";
 
 const Post = () => {
-  return <div>Post</div>;
+  return (
+    <div className="bg-white rounded-[1rem] px-5 py-4 mt-4">
+      {/* Header */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center ">
+          <div className="w-12 h-12">
+            <Image src={guy} className="rounded-full" />
+          </div>
+          <div className="ml-3">
+            <p className="font-bold ">Joe Doe</p>
+            <div className="flex">
+              <p className="text-xs">3 hours ago &#8226;</p>
+              <BiWorld className="ml-1 shrink-0" />
+            </div>
+          </div>
+        </div>
+
+        <div className="w-10 h-10">
+          <Image src={dots} />
+        </div>
+      </div>
+      {/* Input */}
+      {/* Image */}
+      {/* number of Likes + buttons */}
+      {/* comment section */}
+      {/* Input */}
+    </div>
+  );
 };
 
 export default Post;
