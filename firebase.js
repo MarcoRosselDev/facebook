a69e5130518fd533a9ef82f112bbb2edd12a3bf0
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-08 02:44:09 -0300

    gogle acount successfull (error on .env just number and letters)

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { getApps, initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -17,7 +17,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-
 const db = getFirestore();
 const storage = getStorage();
 
