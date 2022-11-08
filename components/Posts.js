82e88f83c25d22f50ed1c59f8c34c9189e052d99
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-08 05:06:14 -0300

    log the comments from db

--- components/Posts.js
+++ components/Posts.js
@@ -1,7 +1,23 @@
-import React from "react";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
+import { db } from "../firebase";
 
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const unSubscribe = onSnapshot(
+      query(collection(db, "posts"), orderBy("timestamp", "desc")),
+      (snapshot) => {
+        setPosts(snapshot.docs);
+      }
+    );
+    return () => {
+      unSubscribe();
+    };
+  }, [db]);
+
   return (
     <div className="w-screen sm:w-full">
       <div className="my-6 max-w-[25rem] sm:max-w-[33rem] mx-auto">
