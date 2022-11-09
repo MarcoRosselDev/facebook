d6d36115b7b8ac03e779f4b67fa78d1bfa9a8296
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-09 04:48:46 -0300

    login header to change of user without gmail

--- components/Header.js
+++ components/Header.js
@@ -1,54 +1,56 @@
-import React from "react";
 import Image from "next/image";
+import React from "react";
 import facebook from "../assets/facebook1.png";
+import user from "../assets/guy7.jpg";
+import nouser from "../assets/nouser.png";
+
 import { MdHome } from "react-icons/md";
 import { FiPlayCircle, FiFlag, FiMessageCircle } from "react-icons/fi";
 import { BsCart3 } from "react-icons/bs";
 import { GrGroup, GrAppsRounded } from "react-icons/gr";
 import { FaBell } from "react-icons/fa";
 import { AiOutlineMessage } from "react-icons/ai";
-import user from "../assets/guy7.jpg";
 import { useSession, signIn, signOut } from "next-auth/react";
-
+import { useRouter } from "next/router";
 const Header = () => {
   const { data: session } = useSession();
+  const router = useRouter();
+  console.log(session);
   return (
     <div className="p-4 flex items-center justify-between border-b lg:px-10">
-      {/* left Side */}
+      {/* LeftSide */}
       <div className="flex items-center mr-2">
-        <div className="w-12 h-12">
-          <Image src={facebook} alt="logo of facebook" key="9"></Image>
+        <div className="w-10 h-10" onClick={() => router.push("/")}>
+          <Image src={facebook} />
         </div>
-        <div className="ml-2">
+        <div className="ml-2 ">
           <input
             type="text"
             placeholder="Search Facebook"
-            className="ml-2 outline-0 bg-[#f2f3f7] p-3 rounded-full pl-4 hidden sm:block"
+            className="outline-0 bg-[#f2f3f7] p-2 rounded-full pl-4 hidden sm:block"
           />
         </div>
       </div>
-      {/* middle */}
-      <div className="flex items-center space-x-7 ml-0">
-        <MdHome className="w-8 h-9" />
-        <FiFlag className="w-7 h-6" />
-        <FiPlayCircle className="w-8 h-7" />
+      {/* Middle */}
+      <div className="flex items-center space-x-7">
+        <MdHome className="w-9 h-9" />
+        <FiFlag className="w-7 h-7" />
+        <FiPlayCircle className="w-7 h-7" />
         <BsCart3 className="w-7 h-7" />
         <GrGroup className="w-7 h-7" />
       </div>
-      {/* Right Side */}
-      <div className="flex space-x-7 items-center ml-0">
-        <div className="md:flex space-x-6 hidden">
+      {/* RightSide */}
+      <div className="flex space-x-6 items-center ml-0">
+        <div className="md:flex space-x-6 hidden ">
           <GrAppsRounded className="w-7 h-7" />
-          <FaBell className="w-7 h-7" />
+          <FaBell className="w-7      h-7" />
           <AiOutlineMessage className="w-7 h-7" />
         </div>
 
-        <div className="w-10 h-10">
+        <div className="w-10 h-10   " onClick={() => signIn()}>
           <img
-            src={session?.user?.image}
+            src={session ? session?.user?.image : nouser.src}
             className="rounded-full"
-            alt="user image"
-            key="8"
           />
         </div>
       </div>
